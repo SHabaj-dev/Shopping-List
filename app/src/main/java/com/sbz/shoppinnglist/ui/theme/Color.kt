e3dac54031f1e7091b307c9d9c18bb94c6caaeec
SHabaj-dev <shabajansari843@gmail.com>
2023-08-06 11:17:40 +0530

    initial commit

---
+++ app/src/main/java/com/sbz/shoppinnglist/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.sbz.shoppinnglist.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xff349AE1)
+val PurpleGrey80 = Color(0xff3A668E)
+val Pink80 = Color(0xff1E3956)
+
+val Purple40 = Color(0xff70CCEA)
+val PurpleGrey40 = Color(0xff349AE1)
+val Pink40 = Color(0xff3A668E)
+
+/*
+val Purple200 = Color(0xff349AE1)
+val Purple500 = Color(0xff3A668E)
+val Purple700 = Color(0xff1E3956)
+val Teal200 = Color(0xff70CCEA)*/
